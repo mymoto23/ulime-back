b9afaa52e511f40c542916395cbada40df3a5e7e
mymoto <mymoto23@naver.com>
2020-05-05 17:39:17 +0900

    delete slash origin

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ app.use(indexRouter);
 app.use(cors({
     credentials: true, // enable set cookie
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    origin: ['http://13.209.211.204:3000/']
+    origin: ['http://13.209.211.204:3000']
   }));
 
 app.get('/', (req, res) => {
