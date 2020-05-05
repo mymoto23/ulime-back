f634f24256b5e155321308b4b01e855487c5347c
mymoto <mymoto23@naver.com>
2020-05-05 17:03:46 +0900

    asd

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     'secret': 'ulimebackhashkey',
-    'mongodbUri': 'mongodb://localhost/mongodb_tutorial',
+    'mongodbUri': 'mongodb+srv://admin:admin@ulime-back-dljbc.mongodb.net/test?retryWrites=true&w=majority',
     'youtubeAPIKey': 'AIzaSyBGstWEDuI5tkz7zR9g5izAlNNtqXL2kRU'
 }
 
