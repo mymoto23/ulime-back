657f608a84208cf05c43a784c0d79b01fdfc0394
mymoto <mymoto23@naver.com>
2020-05-07 08:14:27 +0900

    ??

---
+++ src/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'secret': 'ulimebackhashkey',
+    'mongodbUri': 'mongodb+srv://admin:admin@ulime-back-dljbc.mongodb.net/test?retryWrites=true&w=majority',
+    'youtubeAPIKey': 'AIzaSyBGstWEDuI5tkz7zR9g5izAlNNtqXL2kRU'
+}
